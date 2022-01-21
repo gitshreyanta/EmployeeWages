264461d31b7450d83fcbc11f1804bc3ef35f5854
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-21 23:18:43 +0530

    [Shreya] MultiCompanies

--- EmpolyeeWages/Emp1.cs
+++ EmpolyeeWages/Emp1.cs
@@ -12,58 +12,59 @@ namespace EmpolyeeWages
         public const int PART_TIME = 1;
         public static int empHrs = 0;
 
-        //Method to Calculate the Wages of Emp
-        public static void Work()
+        //Method to calculate wage for multiple companies
+        public void Work(string company, int EmpRatePerHr, int MaxWorkingDays, int MaxWorkingHrs)
         {
 
             int totalWage = 0;
             int empWage;
-            const int EMP_RATE_PER_HR = 20;
-            const int NUM_OF_WORKING_DAYS = 20;
-            const int MAX_WORKING_HRS = 100;
             int totalWorkingdays = 0;
             int empWorkingHrs = 0;
+
             //Random obj created
             Random random = new Random();
-            //iterating for Maximum working days and hours
-            while (totalWorkingdays <= NUM_OF_WORKING_DAYS && empWorkingHrs <= MAX_WORKING_HRS)
+            //iterating Working hrs of emp for max hours and days 
+            while (empWorkingHrs < MaxWorkingHrs && totalWorkingdays <= MaxWorkingDays)
             {
                 int randomInput = random.Next(0, 3);
-
-                //calling method to get  the working hours
+                //calling method to get working hours of employee
                 CalculateWorkingHrs(randomInput);
-                empWage = EMP_RATE_PER_HR * empHrs;
-                //Console.WriteLine(" Total Employee wage for Days {0} is {1}", totalWorkingdays, empWage);
+                empWage = EmpRatePerHr * empHrs;
+                //Console.WriteLine("Employee wage for Day {0} is {1}", days, empWage);
                 totalWage = totalWage + empWage;
                 empWorkingHrs = empWorkingHrs + empHrs;
+
+                if (empWorkingHrs > MaxWorkingHrs)
+                {
+
+                    empWorkingHrs = MaxWorkingHrs;
+                    break;
+                }
                 totalWorkingdays++;
 
             }
-            Console.WriteLine("total wage for {0} Days and hrs:{1} is:{2} ", NUM_OF_WORKING_DAYS, empWorkingHrs, totalWage);
+            Console.WriteLine("{0} Employee worked for {1}days / {2}days and {3}hours / {4}hours.Total Employee wage is:{5} ", company, totalWorkingdays , MaxWorkingDays, empWorkingHrs, MaxWorkingHrs, totalWage);
         }
 
-
-        //method to calculate emp hours
+        //method to get working hours of employee
         public static void CalculateWorkingHrs(int randomInput)
         {
-            //switch case to check emp present or not
+
             switch (randomInput)
             {
                 case FULL_TIME:
                     empHrs = 8;
-                    //Console.WriteLine("FullTime Employee is present");
+                    //Console.WriteLine("FullTime Employee is present "+empHrs);
                     break;
                 case PART_TIME:
                     empHrs = 4;
-                    //Console.WriteLine("PartTime Employee is present");
+                    //Console.WriteLine("PartTime Employee is present " +empHrs);
                     break;
                 default:
                     empHrs = 0;
-                    //Console.WriteLine("Employee is absent");
+                    //Console.WriteLine("Employee is absent " +empHrs);
                     break;
             }
         }
-
-
     }
 }
--- EmpolyeeWages/Program.cs
+++ EmpolyeeWages/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmpolyeeWages;
 Console.WriteLine("Hello, World!");
-Emp1.Work();
+Emp1 emp1 = new Emp1();
+Emp1 emp2 = new Emp1();
+emp1.Work( "Airtel" , 15, 25, 150 );
+emp2.Work( "Pepsi" , 15, 20, 140);
+
