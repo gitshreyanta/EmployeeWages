8b3d9985f678f55675b3a029b7d047f1a358a612
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-21 17:50:21 +0530

    [shreya] UC2_Employeefulltime

--- EmpolyeeWages/Emp1.cs
+++ EmpolyeeWages/Emp1.cs
@@ -6,23 +6,40 @@ using System.Threading.Tasks;
 
 namespace EmpolyeeWages
 {
-    public class Emp1
+    public class EmpWage
     {
-        public static void IffElse()
+        public int empHrs;
+        public int empTotalWage;
+
+        public void IffElse()
         {
+            int wagePerHrs = 20;
             Random random = new Random();
 
+            //To check emp present or not
             int RandomNumber = random.Next(0, 2);
 
 
             if (RandomNumber == 0)
             {
                 Console.WriteLine("Emplyee is Present");
+                //To check full time or not
+                int check = random.Next(0, 2);
+                if (check == 1)
+                {
+                    Console.WriteLine("Employe is present for full time");
+                    empHrs = 8;
+
+                }
             }
             else
             {
                 Console.WriteLine("Emplyee is absent");
             }
+            //calculating total emp wage
+            empTotalWage = wagePerHrs * empHrs;
+            //printing total emp wage
+            Console.WriteLine("EmployeWage Total Wage Is = " + empTotalWage);
 
         }
     }
