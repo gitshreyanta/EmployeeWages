8f46e84eba849da398a813b777bcaf33e06ecbde
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-21 20:43:03 +0530

    [shreya] 20 days

--- EmpolyeeWages/Emp1.cs
+++ EmpolyeeWages/Emp1.cs
@@ -10,14 +10,14 @@ namespace EmpolyeeWages
         const int PART_TIME = 1;
         const int EMP_RATE_PER_HOUR = 20;
         const int NUM_OF_WORKING_DAYS = 20;
-        const int MAX_HRS_IN_MONTH = 100;
+        //const int MAX_HRS_IN_MONTH = 100;
 
         public void Work()
         {
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-            //to check condition of 100 working hrs or 20 days
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            //to check condition of 20 days
+            while (totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -42,11 +42,11 @@ namespace EmpolyeeWages
                 totalEmpHrs += empHrs;
                 //Console.WriteLine("Day: " + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
-            // Printing totalworking days and totalworking hrs
-            Console.WriteLine("Day: " + totalWorkingDays + " Emp Hrs : " + totalEmpHrs);
+            // Printing totalworking days in a month
+            Console.WriteLine("Total working Days: " + totalWorkingDays );
             //printing total emp wage
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            Console.WriteLine("Total Emp Wage for 1 month : " + totalEmpWage);
         }
 
 
