c336cc7d03cfea2bdc02f562a9f5244300a61716
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-21 17:23:12 +0530

    [shreya] EmpIfelse

---
+++ EmpolyeeWages/Emp1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpolyeeWages
+{
+    public class Emp1
+    {
+        public static void IffElse()
+        {
+            Random random = new Random();
+
+            int RandomNumber = random.Next(0, 2);
+
+
+            if (RandomNumber == 0)
+            {
+                Console.WriteLine("Emplyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyee is absent");
+            }
+
+        }
+    }
+}
---
+++ EmpolyeeWages/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmpolyeeWages;
+Console.WriteLine("Hello, World!");
+Emp1.IffElse();
